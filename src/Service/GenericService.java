f0577640c05d88ca7ec7115393c03c67515b960d
donramones <agusrivarolaa00@gmail.com>
2025-11-13 00:24:27 -0300

    10ok

--- src/Service/GenericService.java
+++ src/Service/GenericService.java
@@ -3,11 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package Service;
+import java.util.List;
 
 /**
  *
  * @author Daniela Nahir Romero
  */
-public interface GenericService {
-    
+public interface GenericService<T> {
+    //interfaz generica que define las operaciones basicas que deben implementar todos los servicios
+    void insertar (T entidades) throws Exception;
+    void actualizar (T entidades) throws Exception;
+    void eliminar (int id)throws Exception;
+    T getByID (int id) throws Exception;
+    List<T> getAll() throws Exception;
+         
 }
