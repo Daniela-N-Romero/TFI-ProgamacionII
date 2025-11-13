0478be3bf9a336c25c3b1778409438271f6104ad
Tevan88 <tevanrivarola@gmail.com>
2025-11-13 09:55:09 -0300

    Listo Base

--- src/Models/Envio.java
+++ src/Models/Envio.java
@@ -4,10 +4,68 @@
  */
 package Models;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Daniela Nahir Romero
  */
-public class Envio {
+public class Envio extends Base{
+    //Definimos los atributos:
+    private String tracking;
+    private double costo;
+    private java.time.LocalDate fechaDespacho;
+    private java.time.LocalDate fechaEstimada;
+    
+    //Constructor completo:
+    public Envio(String tracking, double costo, LocalDate fechaDespacho, LocalDate fechaEstimada, int id) {
+        super(id, false); //Llama al constructor de Base con eliminado=false
+        this.tracking = tracking;
+        this.costo = costo;
+        this.fechaDespacho = fechaDespacho;
+        this.fechaEstimada = fechaEstimada;
+    }
+    
+    //Constructor por defecto:
+    public Envio() {
+        super();
+    }
+    
+    //Getters y Setters
+
+    public String getTracking() {
+        return tracking;
+    }
+
+    public void setTracking(String tracking) {
+        this.tracking = tracking;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public LocalDate getFechaDespacho() {
+        return fechaDespacho;
+    }
+
+    public void setFechaDespacho(LocalDate fechaDespacho) {
+        this.fechaDespacho = fechaDespacho;
+    }
+
+    public LocalDate getFechaEstimada() {
+        return fechaEstimada;
+    }
+
+    public void setFechaEstimada(LocalDate fechaEstimada) {
+        this.fechaEstimada = fechaEstimada;
+    }
+    
+    
+           
     
 }
--- src/Models/Pedido.java
+++ src/Models/Pedido.java
@@ -4,11 +4,77 @@
  */
 package Models;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Daniela Nahir Romero
  */
-public class Pedido {
+public class Pedido extends Base{
+    //Definimos los atributos:
     private String numero;
+    private java.time.LocalDate fecha;
+    private String clienteNombre;
+    private String total;
+
+    //Constructor completo:
+    public Pedido(String numero, LocalDate fecha, String clienteNombre, String total, int id) {
+        super(id, false); // Llama al constructor de Base con eliminado=false
+        this.numero = numero;
+        this.fecha = fecha;
+        this.clienteNombre = clienteNombre;
+        this.total = total;
+    }
+    
+    //Constructor por defecto:
+    public Pedido() {
+        super();
+    }
+    
+    //Getters y Setters
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getClienteNombre() {
+        return clienteNombre;
+    }
+
+    public void setClienteNombre(String clienteNombre) {
+        this.clienteNombre = clienteNombre;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    //Generamos el toString:
+    @Override
+    public String toString() {
+        return "Pedido{" + "numero=" + numero + ", fecha=" + fecha + ", clienteNombre=" + clienteNombre + ", total=" + total + '}';
+    }
+    
+    
+    
+    
+    
+    
     
 }
