9c5bc3facfb723b13cb4544c7b66f6f3ee4ab3df
Daniela Romero <danielanahirromero@gmail.com>
2025-11-18 22:51:06 -0300

    Verificando Creación de Tablas

--- src/Config/init.sql
+++ src/Config/init.sql
@@ -2,26 +2,26 @@
 
 -- DDL: Creación de tablas. Usar IF NOT EXISTS para evitar errores de duplicidad.
 CREATE TABLE IF NOT EXISTS pedidos (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    numero_pedido VARCHAR(10) NOT NULL UNIQUE,
-    fecha DATE NOT NULL,
-    cliente_nombre VARCHAR(100) NOT NULL,
-    total DECIMAL(10, 2) NOT NULL,
-    estado VARCHAR(20) NOT NULL, -- Ej: NUEVO, FACTURADO, ENVIADO
-    eliminado BOOLEAN NOT NULL DEFAULT 0
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    eliminado TINYINT(1) NOT NULL DEFAULT 0,
+    numero_pedido VARCHAR(20) NOT NULL UNIQUE,
+    fecha datetime NOT NULL,
+    cliente_nombre VARCHAR(120) NOT NULL,
+    total DECIMAL(12,2) NOT NULL,
+    estado ENUM('NUEVO', 'FACTURADO', 'ENVIADO')
 );
 
 CREATE TABLE IF NOT EXISTS envios (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    tracking VARCHAR(10) NOT NULL UNIQUE,
-    empresa VARCHAR(50) NOT NULL,
-    tipo VARCHAR(20) NOT NULL, -- Ej: ESTANDAR, EXPRESS
-    costo DECIMAL(10, 2) NOT NULL,
-    fechaDespacho DATE NOT NULL,
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    eliminado TINYINT(1) NOT NULL DEFAULT 0,
+    tracking VARCHAR(40) UNIQUE,
+    empresa ENUM('ANDREANI', 'OCA', 'CORREO_ARG'),
+    tipo ENUM('ESTANDAR', 'EXPRESS'),
+    costo DECIMAL(10,2),
+    fechaDespacho DATE,
     fechaEstimada DATE,
-    estado VARCHAR(20) NOT NULL, -- Ej: EN_PREPARACION, EN_TRANSITO, ENTREGADO
-    id_pedido BIGINT NOT NULL UNIQUE, -- Clave foránea 1:1 con pedidos
-    eliminado BOOLEAN NOT NULL DEFAULT 0,
+    estado ENUM('EN_PREPARACION', 'EN_TRANSITO', 'ENTREGADO'),
+    `id_pedido` BIGINT NOT NULL,
     FOREIGN KEY (id_pedido) REFERENCES pedidos(id)
 );
 
